00bdd7f89f1362b026b33167eb1105918672f251
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-14 16:54:49 -0600

    added objects in JournalDataProvider

---
+++ scripts/JournalDataProvider.js
@@ -0,0 +1,44 @@
+/*
+ *   Journal data provider for Daily Journal application
+ *
+ *      Holds the raw data about each entry and exports
+ *      functions that other modules can use to filter
+ *      the entries for different purposes.
+ */
+
+// This is the original data.
+const journal = [
+    {
+        id: 1,
+        date: "07/24/2025",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Amazing"
+    },
+    {
+        id: 2,
+        date: "07/26/2025",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+        mood: "Taylor Swift after a breakup"
+    },
+    {
+        id: 3,
+        date: "08/04/2025",
+        concept: "JavaScript Functions",
+        entry: "I learned more about functions, and what they can be used for to streamline your websites data",
+        mood: "Okay"
+    }
+]
+
+/*
+    You export a function that provides a version of the
+    raw data in the format that you want
+*/
+export const useJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+    return sortedByDate
+}
