e0fc953bea2355c0e9869a4b124254cfaa07767e
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-29 11:35:50 -0600

    Used Fetch to JournalDataProvider.js

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -6,37 +6,25 @@
  *      the entries for different purposes.
  */
 
-// This is the original data.
-const journal = [
-    {
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Amazing"
-    },
-    {
-        id: 2,
-        date: "07/26/2025",
-        concept: "Complex Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Taylor Swift after a breakup"
-    },
-    {
-        id: 3,
-        date: "08/04/2025",
-        concept: "JavaScript Functions",
-        entry: "I learned more about functions, and what they can be used for to streamline your websites data",
-        mood: "Okay"
-    }
-]
+// use fetch() to get data from API
+let journalEntry = []
 
+
+
+export const getEntries = () => {
+    return fetch("http://localhost:8088/entries")
+    .then(response => response.json())
+    .then(entries => {
+        journalEntry = entries
+    })
+}
+// debugger
 /*
     You export a function that provides a version of the
     raw data in the format that you want
 */
 export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
+    const sortedByDate = journalEntry.sort(
         (currentEntry, nextEntry) =>
             Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
     )
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
 import {EntryListComponent} from './JournalEntryList.js'
 
 EntryListComponent()
+
+import {getEntries} from "./JournalDataProvider.js"
+
+
+
+// useJournalEntry()
+// console.log('useJournalEntry: ', useJournalEntry);
+
+const entryArray = getEntries()
+console.log('getEntries: ', entryArray);
